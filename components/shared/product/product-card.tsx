a8feddfe62b7a01baac5d295cdb31ffb6869e315
MehdiKKoohneshin0 <mehdikoohneshin2002@gmail.com>
2026-06-29 16:21:44 +0330

    added product-card component for cleaner code and apply proper colors to texts based on dark color

---
+++ components/shared/product/product-card.tsx
@@ -0,0 +1,47 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import ProductPrice from "./product-price";
+
+import type { Product } from "./product-listing";
+
+const ProductCard = ({ product }: { product: Product }) => {
+  return (
+    <Card key={product.slug}>
+      <CardHeader>
+        <Link
+          href={`/products/${product.slug}`}
+          className="w-full flex justify-center"
+        >
+          <Image
+            src={product.images[0]}
+            className="h-full"
+            width={300}
+            height={300}
+            alt={product.slug}
+          />
+        </Link>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4 justify-between flex-1">
+        <div>
+          <span className="text-xs text-gray-500 dark:text-gray-400">
+            {product.brand}
+          </span>
+          <Link href={`/products/${product.slug}`}>
+            <h3 className="font-bold text-sm text-gray-600 dark:text-gray-50">
+              {product.name}
+            </h3>
+          </Link>
+        </div>
+        <div className="flex-between text-gray-500 dark:text-gray-50">
+          <span className="text-xs dark:text-gray-400">
+            {product.rating} stars
+          </span>
+          <ProductPrice price={product.price} />
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductCard;
--- components/shared/product/product-listing.tsx
+++ components/shared/product/product-listing.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import ProductPrice from "./product-price";
+import ProductCard from "./product-card";
 
 export type Product = {
   name: string;
@@ -41,36 +42,7 @@ const ProductListing = ({
       {title && <h2 className="h2-bold mb-4">{title}</h2>}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {limitedProducts.map((product: Product) => (
-          <Card key={product.slug}>
-            <CardHeader>
-              <Link
-                href={`/products/${product.slug}`}
-                className="w-full flex justify-center"
-              >
-                <Image
-                  src={product.images[0]}
-                  className="h-full"
-                  width={300}
-                  height={300}
-                  alt={product.slug}
-                />
-              </Link>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-4 justify-between flex-1">
-              <div>
-                <span className="text-xs text-gray-500">{product.brand}</span>
-                <Link href={`/products/${product.slug}`}>
-                  <h3 className="font-bold text-sm text-gray-600">
-                    {product.name}
-                  </h3>
-                </Link>
-              </div>
-              <div className="flex-between text-gray-500">
-                <span className="text-xs">{product.rating} stars</span>
-                <ProductPrice price={product.price} />
-              </div>
-            </CardContent>
-          </Card>
+          <ProductCard key={product.slug} product={product} />
         ))}
       </div>
     </>
