41c9fb47f9731f3e6db3181094dc1645d49d6c2a
MehdiKKoohneshin0 <mehdikoohneshin2002@gmail.com>
2026-07-01 16:53:40 +0330

    change in prisma instance to convert Decimal type to String

--- db/prisma-instance.ts
+++ db/prisma-instance.ts
@@ -6,6 +6,19 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 
-export const prisma = new PrismaClient({
-  adapter,
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
 });
